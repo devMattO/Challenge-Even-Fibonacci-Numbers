359cd2b6da8121c7b7f2fa60240f011657014596
Mattdre <olsen_matthew@yahoo.com>
2016-06-13 11:07:56 -1000

    forgot to commit, 3 tests currently passing

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,9 +7,24 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var nextNumber = [];
+  var firstNum = 1;
+  var secondNum = 2;
+  nextNumber.push(secondNum);
 
-  // do your work here
 
+  for (var i = 1; secondNum < maxFibValue; i++ ) {
+    var fn = firstNum; //assigning first num to fn variable
+    firstNum = secondNum; //assigning firstNum value to second Num
+    secondNum = secondNum + fn; //secondNum will equal itself plus the previous num
+    if(secondNum % 2 === 0){ //if the second number is even
+      nextNumber.push(secondNum); //push into nextNum array
+    }
+  }
+  for (var j = 0; j < nextNumber.length; j++) {
+    sum = nextNumber[j] + sum;
+  }
+  console.log(sum);
   return sum;
 }
 
@@ -18,7 +33,6 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
-
 
   //do your work here
 
